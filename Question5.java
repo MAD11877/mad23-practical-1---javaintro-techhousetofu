e2ae9ccd6f3025613033931c41f23abeb320863e
techhousetofu <70971175+techhousetofu@users.noreply.github.com>
2023-05-02 11:38:51 +0800

    Find mode

--- Question5.java
+++ Question5.java
@@ -26,7 +26,38 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
+    // User input
     Scanner in = new Scanner(System.in);
+    System.out.print("How many numbers would you like to enter: ");
+    int num = in.nextInt();
+
+    // Array (eg. [2, 1, 3, 4, 3])
+    int num_arr[] = new int[num];
+    for (int i = 0; i < num; i++) {
+      System.out.format("No. %d: ", i+1);
+      int n = in.nextInt();
+      num_arr[i] = n;
+    }
+
+    // Loop
+    int count, max_count = 0, mode = 0;
+    for (int j = 0; j < num_arr.length; j++) {
+      count = 0;
+      for (int k = 0; k < num_arr.length; k++) {
+        if (num_arr[j] == num_arr[k]) {
+          count += 1;
+        }
+      }
+      if (count > max_count) {
+        mode = num_arr[j];
+        max_count = count;
+      }
+    }
+
+    // Print mode
+    System.out.println("Highest Frequency (mode): "+mode);
+
+    in.close();
     
   }
 }
